d6ce65a40e44018686307e921d8907c4caef89dc
guudewie <philippthiessen12@gmail.com>
2024-08-08 20:45:10 +0200

    set up database

---
+++ db/queries.js
@@ -0,0 +1,19 @@
+const pool = require("./pool");
+
+// CREATE
+async function createCategory() {}
+async function createItem() {}
+
+// READ
+async function getAllCategories() {}
+async function getAllItems() {}
+
+async function getAllItemsOfCategory() {}
+
+// DELETE
+async function deleteCategory() {}
+async function deleteItem() {}
+
+// UPDATE
+async function updateItem() {}
+async function updateCategory() {}
