40cc489b72aee275006b03e9074b34d55d18cacc
guudewie <philippthiessen12@gmail.com>
2024-08-08 18:15:46 +0200

    set up express

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello, world!"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
