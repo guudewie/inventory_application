32aaa20edc336cb2c2b5e06be68cfc2e90a8c724
guudewie <philippthiessen12@gmail.com>
2024-08-12 15:12:29 +0200

    add dummy controller functions

--- controllers/securityController.js
+++ controllers/securityController.js
@@ -2,28 +2,28 @@ const asyncHandler = require("express-async-handler");
 const db = require("../db/queries");
 
 const getAllSecurities = asyncHandler(async (req, res, next) => {
-  res.render("Endpoint not available");
+  res.send("Endpoint not available");
 });
 const getSecurityDetail = asyncHandler(async (req, res, next) => {
-  res.render("Endpoint not available");
+  res.send("Endpoint not available");
 });
 const getCreateForm = asyncHandler(async (req, res, next) => {
-  res.render("Endpoint not available");
+  res.send("Endpoint not available");
 });
 const createSecurity = asyncHandler(async (req, res, next) => {
-  res.render("Endpoint not available");
+  res.send("Endpoint not available");
 });
 const getUpdateForm = asyncHandler(async (req, res, next) => {
-  res.render("Endpoint not available");
+  res.send("Endpoint not available");
 });
 const updateSecurity = asyncHandler(async (req, res, next) => {
-  res.render("Endpoint not available");
+  res.send("Endpoint not available");
 });
 const getDeleteConfirmation = asyncHandler(async (req, res, next) => {
-  res.render("Endpoint not available");
+  res.send("Endpoint not available");
 });
 const deleteSecurity = asyncHandler(async (req, res, next) => {
-  res.render("Endpoint not available");
+  res.send("Endpoint not available");
 });
 
 module.exports = {
