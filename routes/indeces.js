562f2114c85a5ce753a18ad5b227dde2016ec932
guudewie <philippthiessen12@gmail.com>
2024-08-12 11:22:51 +0200

    remove /indices prefix in index routes

--- routes/indeces.js
+++ routes/indeces.js
@@ -4,32 +4,32 @@ const indices = require("../controllers/indexController");
 
 /// GET INDICES ///
 
-// GET /indices - Get all indices
-router.get("/indices", indices.getAllIndices);
+// GET / - Get all indices
+router.get("/", indices.getAllIndices);
 
-// GET /indices/:id - Get index detail
-router.get("/indices/:id", indices.getIndexDetail);
+// GET /:id - Get index detail
+router.get("/:id", indices.getIndexDetail);
 
 /// CREATE INDICES ///
 
-// GET /indices/new - Display create index form
-router.get("/indices/:id/new", indices.getCreateForm);
+// GET /new - Display create index form
+router.get("/:id/new", indices.getCreateForm);
 
-// POST /indices/new - Process index creattion
-router.post("/indices/:id/new", indices.createIndex);
+// POST /new - Process index creattion
+router.post("/:id/new", indices.createIndex);
 
 /// UPDATE INDICES ///
 
-// GET /indices/:id/edit - Display update form
-router.get("/indices/:id/edit", indices.getUpdateForm);
+// GET /:id/edit - Display update form
+router.get("/:id/edit", indices.getUpdateForm);
 
-// PUT /indices/:id - Process index update
-router.put("/indices/:id", indices.updateIndex);
+// PUT /:id - Process index update
+router.put("/:id", indices.updateIndex);
 
 /// DELETE INDICES ///
 
-// GET /indices/:id/delete - Display delete confirmation
-router.get("/indices/:id/delete", indices.getDeleteConfirmation);
+// GET /:id/delete - Display delete confirmation
+router.get("/:id/delete", indices.getDeleteConfirmation);
 
-// DELETE /indices/:id - Process index deletion
-router.delete("/indices/:id", indices.deleteIndex);
+// DELETE /:id - Process index deletion
+router.delete("/:id", indices.deleteIndex);
