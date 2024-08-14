7a055d5bc5f7739eef7a909ee5ccfd8212f791f9
guudewie <philippthiessen12@gmail.com>
2024-08-14 15:51:25 +0200

    add createIndex middleware functions

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const db = require("../db/queries");
+const { body, validationResult } = require("express-validator");
 
 const type = "index";
 
@@ -13,11 +14,41 @@ const getIndexDetail = asyncHandler(async (req, res, next) => {
   res.render("partials/indexDetail", { index: index[0], type: type });
 });
 const getCreateForm = asyncHandler(async (req, res, next) => {
-  res.send("Endpoint not available");
+  const securities = await db.getAllSecurities();
+  const securityTypes = await db.getAllSecurityTypes();
+  res.render("partials/indexForm", {
+    securities: securities,
+    securityTypes: securityTypes,
+  });
 });
-const createIndex = asyncHandler(async (req, res, next) => {
-  res.send("Endpoint not available");
-});
+const createIndex = [
+  body("name").trim().escape(),
+  body("description").trim().escape(),
+  body("ticker").trim().escape(),
+  asyncHandler(async (req, res, next) => {
+    const { name, ticker, description, selectedSecurities } = req.body;
+    const securityTypeId = parseInt(req.body.securityType);
+    const securities = selectedSecurities.split(",");
+
+    const newIndexId = await db.createIndex(
+      name,
+      description,
+      ticker,
+      securityTypeId,
+    );
+    console.log(newIndexId);
+    if (securities.length) {
+      await Promise.all(
+        securities.map((securityId) => {
+          console.log("secuityId", securityId);
+          console.log("IndexId", newIndexId);
+          db.createIndexSecurity(securityId, newIndexId);
+        }),
+      );
+    }
+    res.redirect(`/index/${newIndexId}`);
+  }),
+];
 const getUpdateForm = asyncHandler(async (req, res, next) => {
   res.send("Endpoint not available");
 });
