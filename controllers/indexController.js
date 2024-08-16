e240c0d2d48d9ff3ae84fda577a18862bd8b960a
guudewie <philippthiessen12@gmail.com>
2024-08-16 11:13:37 +0200

    add index security relation update logic

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -17,8 +17,9 @@ const getCreateForm = asyncHandler(async (req, res, next) => {
   const securities = await db.getAllSecurities();
   const securityTypes = await db.getAllSecurityTypes();
   res.render("partials/indexForm", {
-    securities: securities,
+    title: "Create Index",
     action: "/index/new",
+    securities: securities,
     securityTypes: securityTypes,
     errors: {},
     formData: {},
@@ -52,6 +53,7 @@ const createIndex = [
 
       return res.status(400).render("partials/indexForm", {
         title: "Create Index",
+        action: "/index/new",
         securities: securities,
         securityTypes: securityTypes,
         errors: errors.mapped(),
@@ -92,22 +94,18 @@ const getUpdateForm = asyncHandler(async (req, res, next) => {
   });
 });
 const updateIndex = [
-  body("name")
-    .trim()
-    .isLength({ min: 1 })
-    .withMessage("Please fill in this field."),
+  body("name").trim().notEmpty().withMessage("Please fill in this field."),
   body("description")
     .trim()
-    .isLength({ min: 1 })
+    .notEmpty()
     .withMessage("Please fill in this field."),
   body("ticker_symbol")
     .trim()
-    .isLength({ min: 1 })
+    .notEmpty()
     .withMessage("Please fill in this field."),
   asyncHandler(async (req, res, next) => {
-    const id = req.params.id;
+    const indexId = req.params.id;
     const { name, ticker_symbol, description, selectedSecurities } = req.body;
-    const securityTypeId = parseInt(req.body.securityType);
     const securities = selectedSecurities.split(",");
     const errors = validationResult(req);
 
@@ -117,7 +115,7 @@ const updateIndex = [
 
       return res.status(400).render("partials/indexForm", {
         title: "Update Index",
-        action: `/index/${id}/edit`,
+        action: `/index/${indexId}/edit`,
         securities: securities,
         securityTypes: securityTypes,
         errors: errors.mapped(),
@@ -125,9 +123,30 @@ const updateIndex = [
       });
     }
 
-    await db.updateIndex(id, name, description, ticker_symbol);
+    // update index
+    await db.updateIndex(indexId, name, description, ticker_symbol);
 
-    res.redirect(`/index/${id}`);
+    // update index security relations
+    const securitiesOfIndex = await db.getSecuritiesOfIndex(indexId);
+    const currentSecurityIds = securitiesOfIndex.map(
+      (relation) => relation.security_id,
+    );
+    const newSecurityIds = securities.map((id) => Number(id));
+
+    const currentSecuritySet = new Set(currentSecurityIds);
+    const newSecuritySet = new Set(newSecurityIds);
+
+    const idsToDelete = currentSecurityIds.filter(
+      (id) => !newSecuritySet.has(id),
+    );
+    const idsToAdd = newSecurityIds.filter((id) => !currentSecuritySet.has(id));
+
+    await Promise.all([
+      ...idsToDelete.map((id) => db.deleteIndexSecurity(id, indexId)),
+      ...idsToAdd.map((id) => db.createIndexSecurity(id, indexId)),
+    ]);
+
+    res.redirect(`/index/${indexId}`);
   }),
 ];
 
